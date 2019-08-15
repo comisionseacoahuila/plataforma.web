a02e91ff98f078e52a73dacf94728e24e6f61b9c
Guillermo Valdés Lozano <guillermo@movimientolibre.com>
2019-08-14 20:47:49 -0500

    Prueba con www.

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 # Sitio web https://www.comisionseacoahuila.mx
-SITEURL = 'https://comisionseacoahuila.mx'
+SITEURL = 'https://www.comisionseacoahuila.mx'
 SITENAME = 'Comisión de Selección del SEA Coahuila'
 SITELOGO = 'theme/images/generic_company.png'
 SITEDESCRIPTION = 'Comisión de Selección SEA Coahuila'
